c878b8332a589e7326f48234f8c1c0c839a6c6b2
JT Archie <jarchie@pivotal.io>
2018-10-17 19:43:15 -0600

    WIP

--- mkdocs-plugins/mkdocs-jinja2/mkdocs_jinja2/__init__.py
+++ mkdocs-plugins/mkdocs-jinja2/mkdocs_jinja2/__init__.py
@@ -1,7 +1,39 @@
 __version__ = '0.0.0'
 
+from jinja2 import Environment, FileSystemLoader, lexer, nodes
+from jinja2.ext import Extension
 from mkdocs.plugins import BasePlugin
-from jinja2 import Environment, FileSystemLoader
+import os
+import re
+
+class CodeSnippetExtension(Extension):
+    tags = set(['code_snippet'])
+
+    def parse(self, parser):
+        lineno = next(parser.stream).lineno
+
+        token     = parser.stream.expect(lexer.TOKEN_STRING)
+        repo_name = nodes.Const(token.value)
+
+        if self.environment.dependent_sections[repo_name]:
+            token     = parser.stream.expect(lexer.TOKEN_STRING)
+            code_name = nodes.Const(token.value)
+
+            repo = self.environment.dependent_sections[repo_name]
+            exclude_dirs = set([".git"])
+            regex = re.compile(r'code_snippet %s start (\w+)\n(.*)code_snippet %s end' % (code_name, code_name))
+
+            for root, dirs, files in os.walk(os.path.join(os.getcwd(), repo['directory']), topdown=True):
+                dirs[:] = [d for d in dirs if d not in exclude]
+
+                for name in files:
+                    path = os.path.join(root, name)
+                    with open(path) as f:
+                        matches = re.match(regex, f.read, re.MULTILINE)
+                        # do something
+
+        else:
+            raise Exception('Dependent section "%s" not defined in mkdocs.yml' % (repo_name))
 
 class JinjaMkDocPlugin(BasePlugin):
     def on_page_markdown(self, markdown, page, config, files):
