1e27effd9a2796dbf2e81fd68cde751ef12943f0
JT Archie <jarchie@pivotal.io>
2018-10-17 18:28:46 -0600

    rename package with underscore

--- mkdocs-plugins/mkdocs-jinja2/setup.py
+++ mkdocs-plugins/mkdocs-jinja2/setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.0.0',
     author='JT Archie',
     author_email='jarchie@pivotal',
-    packages=['mkdocs-jinja2'],
+    packages=['mkdocs_jinja2'],
     description='Uses jinja2 and file system loader (for include and partials)',
     install_requires=[],
     entry_points={
         'mkdocs.plugins': [
-            'jinja2 = mkdocs-jinja:JinjaMkDocPlugin',
+            'jinja2 = mkdocs_jinja:JinjaMkDocPlugin',
         ]
     }
 )
