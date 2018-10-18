7f26a081f6b94711ce4c9c8703f3684a4820d512
JT Archie <jarchie@pivotal.io>
2018-10-17 18:25:08 -0600

    use pre markdown rendered text

--- mkdocs-plugins/mkdocs-jinja2/mkdocs-jinja2/__init__.py
+++ mkdocs-plugins/mkdocs-jinja2/mkdocs-jinja2/__init__.py
@@ -4,9 +4,6 @@ from mkdocs.plugins import BasePlugin
 from jinja2 import Environment, FileSystemLoader
 
 class JinjaMkDocPlugin(BasePlugin):
-    def on_page_content(self, html, page, config, files):
+    def on_page_markdown(self, markdown, page, config, _):
         env = Environment(loader=FileSystemLoader(config['docs_dir']))
-        return env.from_string(html).render(
-                config=config,
-                files=files
-                )
+        return env.from_string(markdown).render(config=config)
