bfb43a7423cf82269820c6e56e89359cd21be064
JT Archie <jarchie@pivotal.io>
2018-10-17 18:27:23 -0600

    fix package name

--- mkdocs-plugins/mkdocs-jinja2/setup.py
+++ mkdocs-plugins/mkdocs-jinja2/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.0',
     author='JT Archie',
     author_email='jarchie@pivotal',
-    packages=['mkdocs_jinja2'],
+    packages=['mkdocs-jinja2'],
     description='Uses jinja2 and file system loader (for include and partials)',
     install_requires=[],
     entry_points={
