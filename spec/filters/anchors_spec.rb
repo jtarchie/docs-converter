688a1ac74ba33944520ad00b3e5f7e154c76386e
JT Archie <jarchie@pivotal.io>
2018-10-30 18:09:13 -0600

    rubocop -a

--- spec/filters/anchors_spec.rb
+++ spec/filters/anchors_spec.rb
@@ -6,16 +6,16 @@ require_relative '../../lib/filters/anchor'
 RSpec.describe 'When filtering anchors' do
   def filter(content)
     filter = Docs::Filters::Anchor.new(
-        path: nil,
-        config: nil,
-        content: content
+      path: nil,
+      config: nil,
+      content: content
     )
     filter.process
   end
 
   it 'does not parse anchors not in headers' do
     [
-        %(<a id="testing">testing</a>),
+      %(<a id="testing">testing</a>),
       %(<a id="testing"></a> testing)
     ].each do |content|
       expect(filter(content)).to eq content
@@ -23,9 +23,9 @@ RSpec.describe 'When filtering anchors' do
   end
 
   it 'does parses anchors in headers' do
-    expect(filter(%{# <a id="testing"></a> testing})).to eq "# testing"
-    expect(filter(%{## <a id="testing"></a> testing})).to eq "## testing"
-    expect(filter(%{# <a id="testing">testing</a>})).to eq "# testing"
-    expect(filter(%{## <a id="testing">testing</a>})).to eq "## testing"
+    expect(filter(%(# <a id="testing"></a> testing))).to eq '# testing'
+    expect(filter(%(## <a id="testing"></a> testing))).to eq '## testing'
+    expect(filter(%(# <a id="testing">testing</a>))).to eq '# testing'
+    expect(filter(%(## <a id="testing">testing</a>))).to eq '## testing'
   end
 end
